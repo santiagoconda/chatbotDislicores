ca4225834614b81ab00baa72675a2b07e2182bca
SANTIAGO CONDA <yonatanpazu2@gmail.com>
2026-02-13 14:48:29 -0500

    curdproducts

--- app/Filament/Resources/Products/ProductResource.php
+++ app/Filament/Resources/Products/ProductResource.php
@@ -15,6 +15,12 @@ use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Components\DatePicker;
+
+
 
 class ProductResource extends Resource
 {
@@ -26,12 +32,92 @@ class ProductResource extends Resource
 
     public static function form(Schema $schema): Schema
     {
-        return ProductForm::configure($schema);
-    }
+        return $schema->components([
+            TextInput::make('name')
+                ->required(),
+            TextInput::make('slug'),
+            TextInput::make('description'),
+            Select::make('category_id')
+                ->relationship('category', 'name')
+                ->searchable()
+                ->preload()
+                ->required()
+                ->createOptionForm([
+                    TextInput::make('name')->required(),
+                    ]),
+
+        Select::make('brand_id')
+            ->relationship('brand', 'name')
+            ->searchable()
+            ->preload()
+            ->required(),
+
+        TextInput::make('alcohol_percentage')
+            ->numeric()
+            ->suffix('%'),
+
+        Select::make('type')
+            ->options([
+                'aguardiente' => 'Aguardiente',
+                'ron' => 'Ron',
+                'whisky' => 'Whisky',
+                'vodka' => 'Vodka',
+                'tequila' => 'Tequila',
+                'cerveza' => 'Cerveza',
+            ])
+            ->required(),
+
+        Select::make('age')
+            ->options([
+                'joven' => 'Joven',
+                'añejo' => 'Añejo',
+                'reserva' => 'Reserva',
+                'extra_añejo' => 'Extra Añejo',
+            ]),
+
+        TextInput::make('origin_country')
+            ->required(),
+
+        TextInput::make('volume')
+            ->integer()
+            ->suffix('ml'),
+
+        TextInput::make('price')
+            ->numeric()
+            ->prefix('$')
+            ->required(),
+
+        TextInput::make('discount_price')
+            ->numeric()
+            ->prefix('$'),
+
+        TextInput::make('stock')
+            ->integer()
+            ->default(0),
+
+        TextInput::make('min_stock')
+            ->integer()
+            ->default(1),
+
+        TextInput::make('meta_title'),
+
+        TextInput::make('meta_keywords'),
+
+        Toggle::make('is_active')->default(true),
+        Toggle::make('is_featured'),
+        Toggle::make('is_new'),
+        Toggle::make('is_on_sale'),
+
+        TextInput::make('views')->integer()->default(0),
+        TextInput::make('rating')->numeric()->step(0.1),
+        TextInput::make('reviews_count')->integer()->default(0),
+        ]);   
+        }
 
     public static function table(Table $table): Table
     {
         return ProductsTable::configure($table);
+
     }
 
     public static function getRelations(): array
